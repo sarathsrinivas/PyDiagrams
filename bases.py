fd7f89afe5454f74cbac6a2874da8d561ab37525
sarath srinivas s <srinix@protonmail.com>
2020-08-04 22:53:59 +0530

    Sampler as object.

--- bases.py
+++ bases.py
@@ -3,8 +3,7 @@ import numpy as np
 import opt_einsum as oen
 import sys
 sys.path.append('..')
-import lib_gpr.sampler as smp
-import lib_pots.pot as pots
+import lib_quadrature.ball as quad
 
 tc.set_default_tensor_type(tc.DoubleTensor)
 
@@ -12,27 +11,27 @@ tc.set_default_tensor_type(tc.DoubleTensor)
 class GPR_EX_CART(object):
     "Stochastic Basis with exchange parametrization."
 
-    def __init__(self, n, kmax, seed):
+    def __init__(self, n_1b, n_2b, kmax, sampler):
 
         self.dim_1b = 1
-        self.k_1b, min_dist = smp.nsample_repulsion(n, tc.tensor([0.0]),
-                                                    tc.tensor([1.0]))
+        self.sampler = sampler
+        self.k_1b, min_dist = self.sampler.sample(n_1b, tc.tensor([0.0]),
+                                                  tc.tensor([1.0]))
         self.k_1b.squeeze_()
 
-        self.dim_2b = 6
+        self.dim_2b = 7
 
         self.kmax = kmax
 
-        mins = tc.full(self.dim_2b, -kmax)
+        mins = tc.full([self.dim_2b], -kmax)
         mins[0] = 0.0
-        maxs = tc.full(self.dim_2b, kmax)
+        maxs = tc.full([self.dim_2b], kmax)
 
-        self.k_2b, min_dist = smp.nsample_repulsion(n, mins, maxs)
+        self.k_2b, min_dist = self.sampler.sample(n_2b, mins, maxs)
 
-        self.dl, self.dlp, self.dl_dlp, slef.P_dl, self.P_dlp = self.invariants(
-            self.k_2b)
+        self.invars = self.invariants(self.k_2b)
 
-        self.k_2b_ex = self.rotate_to_dlp()
+        self.k_2b_ex = self.rotate_to_dlp(self.k_2b)
 
     def invariants(self, k_2b):
 
@@ -54,42 +53,51 @@ class GPR_EX_CART(object):
 
         return dl, dlp, P, dl_dlp, P_dl, P_dlp
 
-    def rotate_to_dlp(self, phi_P=0.4):
-        phi_dl = tc.acos(
-            (tc.cos(self.P_dl) - tc.cos(self.P_dlp) * tc.cos(self.dl_dlp)) /
-            (tc.sin(self.P_dlp) * tc.sin(self.dl_dlp))) + phi_P
+    def rotate_to_dlp(self, k_2b, phi_P=0.4):
 
-        k_2b_dlp = tc.empty_like(self.k_2b)
+        dl, dlp, P, dl_dlp, P_dl, P_dlp = self.invariants(k_2b)
 
-        k_2b_dlp[:, 0] = self.dl.mul(phi_dl.cos()).mul_(self.dl_dlp.sin())
-        k_2b_dlp[:, 1] = self.dl.mul(phi_dl.cos()).mul_(self.dl_dlp.sin())
-        k_2b_dlp[:, 2] = self.dl.mul(dl_dlp.cos())
-        k_2b_dlp[:, 3] = self.P.mul(np.cos(phi_P)).mul_(self.P_dlp.sin())
-        k_2b_dlp[:, 4] = self.P.mul(np.sin(phi_P)).mul_(self.P_dlp.sin())
-        k_2b_dlp[:, 5] = self.P.mul(self.P_dlp.cos())
+        phi_dl = tc.acos((tc.cos(P_dl) - tc.cos(P_dlp) * tc.cos(dl_dlp)) /
+                         (tc.sin(P_dlp) * tc.sin(dl_dlp))) + phi_P
+
+        k_2b_dlp = tc.empty_like(k_2b)
+
+        k_2b_dlp[:, 0] = dl.mul(phi_dl.cos()).mul_(dl_dlp.sin())
+        k_2b_dlp[:, 1] = dl.mul(phi_dl.cos()).mul_(dl_dlp.sin())
+        k_2b_dlp[:, 2] = dl.mul(dl_dlp.cos())
+        k_2b_dlp[:, 3] = P.mul(np.cos(phi_P)).mul_(P_dlp.sin())
+        k_2b_dlp[:, 4] = P.mul(np.sin(phi_P)).mul_(P_dlp.sin())
+        k_2b_dlp[:, 5] = P.mul(P_dlp.cos())
 
         return k_2b_dlp
 
-    def get_1b_op(self, k_1b, op=None, **kwargs):
-        return op(k_1b, **kwargs)
+    def get_1b_op(self, k_1b, op_1b=None, op_1b_args=None):
+        return op_1b(k_1b, **op_1b_args)
 
-    def get_2b_op(self, k_2b, pot=None, **kwargs):
+    def get_2b_op(self, k_2b, pot=None, pot_args=None):
 
-        invar = self.invariants(k_2b)
+        invar = self.invariants(k_2b.reshape(-1, self.dim_2b))
 
-        v = pot(*invar, self.kmax, **kwargs)
+        v = pot(*invar, self.kmax, **pot_args)
 
         return v
 
-    def normal_order_1b(self, k_1b, kf, op_1b=None, op_2b=None, nq=20,
-                        nleb=15):
+    def normal_order_1b(self,
+                        k_1b,
+                        kf,
+                        nq=20,
+                        nleb=15,
+                        op_1b=None,
+                        op_1b_args=None,
+                        op_2b=None,
+                        op_2b_args=None):
 
-        q, th_q, phi_q, w = quad.ball_quad(rmax=kf, nr=nq, nleb=nleb)
+        q, th_q, phi_q, wt = quad.ball_quad(rmax=kf, nr=nq, nleb=nleb)
 
         qx, qy, qz = quad.sph_to_cart(q, th_q, phi_q)
 
         tmp = tc.zeros_like(self.k_1b)
-        dlz = tc.zeros(n, nq)
+        dlz = tc.zeros(self.k_2b.shape[0], qx.shape[0])
         dlpx = (tmp - qx).mul_(0.5)
         dlpy = (tmp - qy).mul_(0.5)
         dlpz = (k_1b - qz).mul_(0.5)
@@ -99,16 +107,26 @@ class GPR_EX_CART(object):
 
         kq_2b = tc.stack((dlz, dlpx, dlpy, dlpz, Px, Py, Pz), -1)
 
+        kq_2b = kq_2b.reshape(-1, self.dim_2b)
+
         invar = self.invariants(kq_2b)
 
-        f = op_1b(k_1b, **kwargs)
-        v = op_2b(*invar, self.kmax, **kwargs)
+        f = op_1b(k_1b, **op_1b_args)
+        v = op_2b(*invar, self.kmax, **op_2b_args)
 
         f_no = f.add_(v.mul_(wt).sum(-1))
 
         return f_no
 
-    def normal_order_0b(self, op_1b=None, op_2b=None, **kwargs):
+    def normal_order_0b(self,
+                        kf,
+                        nq=20,
+                        nleb=15,
+                        op_1b=None,
+                        op_1b_args=None,
+                        op_2b=None,
+                        op_2b_args=None):
+
         q, th_q, phi_q, wt = quad.ball_quad(rmax=kf, nr=nq, nleb=nleb)
         qx, qy, qz = quad.sph_to_cart(q, th_q, phi_q)
 
@@ -125,14 +143,14 @@ class GPR_EX_CART(object):
 
         kq_2b = tc.stack((dlz, dlpx, dlpy, dlpz, Px, Py, Pz), -1)
 
-        invar = self.invariants(kq_2b)
+        invar = self.invariants(kq_2b.reshape(-1, self.dim_2b))
 
         k_1b, wt_1b = quad.gauss_legendre(nq, a=0, b=kf)
 
-        f = op_1b(k_1b, **kwargs)
-        v = op_2b(*invar, self.kmax, **kwargs)
+        f = op_1b(k_1b, **op_1b_args)
+        v = op_2b(*invar, self.kmax, **op_2b_args)
 
-        v_int = oen.contract('i,j,ij->', wt, wt, v)
+        v_int = oen.contract('i,j,ij->', wt, wt, v.reshape(n, n))
 
         e_no = tc.dot(f, wt_1b) + 0.5 * v_int
 
