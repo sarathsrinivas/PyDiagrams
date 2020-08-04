c781a854c093e31d72dedb798a4c00d7e328731c
sarath srinivas s <srinix@protonmail.com>
2020-08-04 22:54:23 +0530

    Cleanes kwargs.

--- ham.py
+++ ham.py
@@ -1,11 +1,19 @@
 import torch as tc
 import numpy as np
+import sys
+sys.path.append('..')
+import lib_gpr as gp
 
 tc.set_default_tensor_type(tc.DoubleTensor)
 
 
 class HAMILTONIAN(object):
-    def __init__(self, basis, op_1b=None, pot=None, **pot_args):
+    def __init__(self,
+                 basis,
+                 op_1b=None,
+                 op_1b_args=None,
+                 pot=None,
+                 pot_args=None):
 
         self.basis = basis
         self.E = 0
@@ -13,17 +21,28 @@ class HAMILTONIAN(object):
             self.F = None
             self.V = None
         else:
-            self.F = basis.get_1b_op(basis.k_1b, op=op_1b, **pot_args)
-            self.V = basis.get_2b_op(basis.k_2b, pot=pot, **pot_args)
-            self.pot_args = pot_args
+            self.op_1b = op_1b
+            self.op_2b = pot
+            self.op_1b_args = op_1b_args
+            self.op_2b_args = pot_args
+            self.F = basis.get_1b_op(basis.k_1b,
+                                     op_1b=op_1b,
+                                     op_1b_args=op_1b_args)
+            self.V = basis.get_2b_op(basis.k_2b, pot=pot, pot_args=pot_args)
 
     def normal_order(self, kf):
-        self.E = basis.normal_order_0b(op_1b=op_1b, op_2b=pot, **self.pot_args)
-        self.f = basis.normal_order_1b(self.basis.k_1b,
-                                       kf,
-                                       op_1b=op_1b,
-                                       op_2b=pot,
-                                       **self.pot_args)
+        self.E = self.basis.normal_order_0b(kf,
+                                            op_1b=self.op_1b,
+                                            op_1b_args=self.op_1b_args,
+                                            op_2b=self.op_2b,
+                                            op_2b_args=self.op_2b_args)
+
+        self.F = self.basis.normal_order_1b(self.basis.k_1b,
+                                            kf,
+                                            op_1b=self.op_1b,
+                                            op_1b_args=self.op_1b_args,
+                                            op_2b=self.op_2b,
+                                            op_2b_args=self.op_2b_args)
 
 
 class HAM_GPR(HAMILTONIAN):
@@ -33,15 +52,20 @@ class HAM_GPR(HAMILTONIAN):
                  cov=None,
                  cov_args=None,
                  op_1b=None,
+                 op_1b_args=None,
                  pot=None,
-                 **pot_args):
+                 pot_args=None):
 
-        super().__init__(basis, op_1b=op_1b, pot=pot, **pot_args)
+        super().__init__(basis,
+                         op_1b=op_1b,
+                         op_1b_args=op_1b_args,
+                         pot=pot,
+                         pot_args=pot_args)
 
         super().normal_order(kf)
 
-        self.F_gp = gp.GPR(self.basis.k_1b, self.F, cov, **cov_args)
-        self.V_gp = gp.GPR(self.basis.k_2b, self.V, cov, **cov_args)
+        self.F_gp = gp.gpr.GPR(self.basis.k_1b, self.F, cov, **cov_args)
+        self.V_gp = gp.gpr.GPR(self.basis.k_2b, self.V, cov, **cov_args)
 
     def train(self, method='CG', jac=True):
         res_F = self.F_gp.train(method=method, jac=jac)
@@ -64,14 +88,25 @@ class HAM_GRBCM(HAM_GPR):
                  cov=None,
                  cov_args=None,
                  op_1b=None,
+                 op_1b_args=None,
                  pot=None,
-                 **pot_args):
+                 pot_args=None):
 
-        HAMILTONIAN.__init__(basis, op_1b=op_1b, pot=pot, **pot_args)
+        HAMILTONIAN.__init__(self,
+                             basis,
+                             op_1b=op_1b,
+                             op_1b_args=op_1b_args,
+                             pot=pot,
+                             pot_args=pot_args)
+
+        self.Fg = basis.get_1b_op(basis.k_1b_g,
+                                  op_1b=op_1b,
+                                  op_1b_args=op_1b_args)
+        self.Vg = basis.get_2b_op(basis.k_2b_g, pot=pot, pot_args=pot_args)
 
         super().normal_order(kf)
 
-        self.F_gp = gp.GRBCM(self.basis.k_1b, self.F, self.basis.k_1b_g,
-                             self.Fg, cov, **cov_args)
-        self.V_gp = gp.GRBCM(self.basis.k_2b, self.V, self.basis.k_2b_g,
-                             self.Vg, cov, **cov_args)
+        self.F_gp = gp.gr_bcm.GRBCM(self.basis.k_1b, self.F, self.basis.k_1b_g,
+                                    self.Fg, cov, **cov_args)
+        self.V_gp = gp.gr_bcm.GRBCM(self.basis.k_2b, self.V, self.basis.k_2b_g,
+                                    self.Vg, cov, **cov_args)
